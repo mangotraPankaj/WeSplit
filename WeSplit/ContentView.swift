923476b099dfd0e14757568699f16271c7a79acb
Pankaj Mangotra <pankaj.mangotra@gmail.com>
2021-06-13 17:57:08 +0530

    Creating the picker and the text field to capture the number of people and the check amount

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,9 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 2
+    
+    let tipPercentages = [10, 15, 20, 25]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number of People", selection: $numberOfPeople) {
+                        ForEach(2..<10) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                Section {
+                    Text("$\(checkAmount)")
+                }
+            }.navigationBarTitle(" WeSplit")
+        }
     }
 }
 
